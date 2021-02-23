11dc1fd3ca6bd3974b2b42e4c4167e33b29fb1f9
Palassis <40472433+MxNxPx@users.noreply.github.com>
2021-02-23 12:28:18 -0500

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -25,10 +25,10 @@ data "template_file" "example2" {
 
 resource "local_file" "example" {
   content  = "${data.template_file.example.rendered} + ${data.template_file.example2.rendered}"
-  filename = "example.txt"
+  filename = "/etc/example.txt"
 }
 
 resource "local_file" "example2" {
   content  = data.template_file.example2.rendered
-  filename = "example2.txt"
+  filename = "/etc/example2.txt"
 }
